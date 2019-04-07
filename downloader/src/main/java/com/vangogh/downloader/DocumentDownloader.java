249128d4de5aba0a67bed9827faaf9a1149b865f
Otniel <otnieldocs@gmail.com>
2019-04-07 22:56:16 +0700

    improve Document Downloader

--- downloader/src/main/java/com/vangogh/downloader/DocumentDownloader.java
+++ downloader/src/main/java/com/vangogh/downloader/DocumentDownloader.java
@@ -1,7 +1,63 @@
 package com.vangogh.downloader;
 
-public class DocumentDownloader extends Downloader {
-    public DocumentDownloader(String url, ResultCallback callback) {
-        super(url, callback);
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class DocumentDownloader extends DownloadManager {
+    private static final int BYTE_SIZE = 1024;
+    private static final int DEFAULT_MAX_THREAD = 10;
+    private static final int DEFAULT_MAX_TOTAL_BYTE = 500*BYTE_SIZE;
+
+    public DocumentDownloader() {
+        super(DEFAULT_MAX_THREAD, DEFAULT_MAX_TOTAL_BYTE);
+    }
+
+    public DocumentDownloader(int maxThread, int maxTotalBytes) {
+        super(maxThread, maxTotalBytes);
+    }
+
+    @Override
+    public void download(String url, Downloader.ResultCallback result) {
+        Downloader downloader = new Downloader(url, result);
+
+        manageDownloader(downloader);
+    }
+
+    public void toJSON(String url, final DocumentResponse response) {
+        download(url, new Downloader.ResultCallback() {
+            @Override
+            public void onStarted(Downloader downloader, String encodedUrl) {
+                if (cachedData.get(encodedUrl) != null) {
+                    cachedData.put(encodedUrl, null);
+                }
+
+                response.onStart();
+            }
+
+            @Override
+            public void onFinished(byte[] data, String encodedUrl) {
+                String rawResponse = new String(data);
+                try {
+                    JSONObject jsonResponse = new JSONObject(rawResponse);
+
+                    response.onSuccess(rawResponse, jsonResponse);
+                } catch (JSONException e) {
+                    response.onFailed(e);
+                }
+            }
+
+            @Override
+            public void onFailed(IOException e) {
+                response.onFailed(e);
+            }
+        });
+    }
+
+    public interface DocumentResponse {
+        void onStart();
+        void onSuccess(String raw, JSONObject jsonObject);
+        void onFailed(Exception e);
     }
 }
