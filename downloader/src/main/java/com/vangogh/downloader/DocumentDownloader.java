168028ba6fe189a9481a4184654d4957e585a343
Otniel <otnieldocs@gmail.com>
2019-04-07 22:31:36 +0700

    create DocumentDownloader

--- downloader/src/main/java/com/vangogh/downloader/DocumentDownloader.java
+++ downloader/src/main/java/com/vangogh/downloader/DocumentDownloader.java
@@ -1,4 +1,7 @@
 package com.vangogh.downloader;
 
 public class DocumentDownloader extends Downloader {
+    public DocumentDownloader(String url, ResultCallback callback) {
+        super(url, callback);
+    }
 }
