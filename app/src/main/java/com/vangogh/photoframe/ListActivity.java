ef928b00c1e27b83041632b89ed65c4860da4df4
Otniel <otnieldocs@gmail.com>
2019-04-08 22:16:12 +0700

    add card layout

--- app/src/main/java/com/vangogh/photoframe/ListActivity.java
+++ app/src/main/java/com/vangogh/photoframe/ListActivity.java
@@ -49,5 +49,11 @@ public class ListActivity extends AppCompatActivity {
         contents.add(new ImageContent("https://www.baanlaesuan.com/app/uploads/2016/09/mood.jpg"));
         contents.add(new ImageContent("https://www.baanlaesuan.com/app/uploads/2016/09/02-03.jpg"));
         contents.add(new ImageContent("https://www.baanlaesuan.com/app/uploads/2016/09/04-5.jpg"));
+        contents.add(new ImageContent("https://i.pinimg.com/originals/bb/b4/a1/bbb4a12b56a15ee9c35a88ea039c3f47.jpg"));
+        contents.add(new ImageContent("https://i.pinimg.com/originals/9f/61/b6/9f61b6c445763fa43a50a1ee73ada5aa.jpg"));
+        contents.add(new ImageContent("https://i.pinimg.com/originals/e8/29/fa/e829fae1a1d8fd41d5612d57ff970e5f.jpg"));
+        contents.add(new ImageContent("https://i.pinimg.com/originals/f0/ab/fa/f0abfa0c5718ee8c5f4d32665cf938e4.jpg"));
+        contents.add(new ImageContent("https://i.pinimg.com/originals/f7/58/7c/f7587ca48f4d8130aa4f2aaa5df8d2b9.jpg"));
+        contents.add(new ImageContent("https://i.pinimg.com/originals/ae/94/8f/ae948f2744e4da30c52c84c2ea122ddd.jpg"));
     }
 }
